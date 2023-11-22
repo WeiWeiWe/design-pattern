456a3aa3574d03ff53de65dda8d9d9b1f3e69726
Wei <aszx70293@gmail.com>
2023-11-22 18:58:32 +0800

    20231122 - 1 - Singleton Pattern.

---
+++ src/SingletonPattern/Main.java
@@ -0,0 +1,22 @@
+package SingletonPattern;
+
+class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2); // true
+    }
+}
